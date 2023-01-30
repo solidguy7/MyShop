c7aabd82598152ee945530c4a187df07476249d4
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-30 13:25:04 +0300

    Add a cart session

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
