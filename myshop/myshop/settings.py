e144f66ea684e35b81e529acef31721810319a05
solidguy7 <kirill15grishkov01@gmail.com>
2023-02-07 11:49:05 +0300

    Add coupons

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -126,7 +126,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 CART_SESSION_ID = 'cart'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 CELERY_BROKER_URL = 'amqp://admin:mypass@rabbitmq:5672//'
 
--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -15,6 +15,7 @@ def payment_process(request):
     if request.method == 'POST':
         success_url = request.build_absolute_uri(reverse('payment:completed'))
         cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
+
         # Stripe checkout session data
         session_data = {
             'mode': 'payment',
@@ -35,8 +36,15 @@ def payment_process(request):
                 },
                 'quantity': item.quantity,
             })
+
+        # Stripe coupon
+        if order.coupon:
+            stripe_coupon = stripe.Coupon.create(name=order.coupon.code, percent_off=order.discount, duration='once')
+            session_data['discounts'] = [{'coupon': stripe_coupon.id}]
+
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
+
         # redirect to Stripe payment form
         return redirect(session.url, code=303)
 
